a25d708f932df16cebbd6eee61b587d60bf8547a
Jared Hanson <jaredhanson@gmail.com>
2017-05-01 11:06:38 -0700

    Add assembly metadata.

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+/*
 exports = module.exports = {
   '': require('./xom/main')
+};
+*/
+
+exports = module.exports = function(id) {
+  try {
+    return require('./xom/' + id);
+  } catch (ex) {
+    if (ex.code == 'MODULE_NOT_FOUND') { return; }
+    throw ex;
+  }
 };
